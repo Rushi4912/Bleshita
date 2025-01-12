8d64d6611f36769752b3096260b551316c0c04fc
Rushi4912 <pawarrushikeshpawar@gmail.com>
2025-01-12 18:02:44 +0530

    fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,9 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}', // Adjust paths based on your project structure
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}', // For `app` directory (Next.js 13+)
+    './pages/**/*.{js,ts,jsx,tsx}', // For `pages` directory
+    './components/**/*.{js,ts,jsx,tsx}', // For `components` directory
   ],
   theme: {
     extend: {},
