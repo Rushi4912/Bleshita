016551afd007e945a44de73b4a31ab786b7b006a
Rushi4912 <pawarrushikeshpawar@gmail.com>
2025-05-03 22:53:05 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -43,8 +43,8 @@ const HomePage: React.FC = () => {
       </section>
 
       {/* Product Features */}
-      <section className="bg-gray-50 pt-8 md:pt-8">
-        <div className="container mx-auto px-4 py-8 md:py-6">
+      <section className="bg-gray-50 pt-4 md:pt-8">
+        <div className="container mx-auto px-4 py-4 md:py-6">
           <ProductFeature
             title="Where Did You Get That?"
             description="If they didn't know about Bleshita, this holiday they will. These standout styles will get more compliments than the wrapping paper you used to gift them."
@@ -54,8 +54,8 @@ const HomePage: React.FC = () => {
         </div>
       </section>
 
-      <section className="bg-gray-50 pt-8 md:pt-8">
-        <div className="container mx-auto px-4 py-8 md:py-6">
+      <section className="bg-gray-50 pt-4 md:pt-8">
+        <div className="container mx-auto px-4 py-4 md:py-6">
           <ProductFeature
             title="Oooh, That Looks So Luxurious"
             description="As lush as the vocals of your favorite holiday tunes. We're talking Grade-A cashmere, premium (responsibly sourced!) leather, and more—all waiting to be wrapped and gifted to the ones you love."
@@ -65,8 +65,8 @@ const HomePage: React.FC = () => {
         </div>
       </section>
 
-      <section className="bg-gray-50 pt-8 md:pt-8">
-        <div className="container mx-auto px-4 py-8 md:py-6">
+      <section className="bg-gray-50 pt-4 md:pt-8">
+        <div className="container mx-auto px-4 py-4 md:py-6">
           <ProductFeature
             title="I Can't Believe It's Not Cashmere"
             description="Finding quality that exceeds your expectations and not your budget—that's a Christmas miracle. Get into cozy and super soft sweaters that look more expensive than they are."
